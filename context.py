cf96c0e3d2ee522ab0c4c3221754fafb36cae5e4
Istvan Albert <istvan.albert@gmail.com>
2014-11-25 15:32:08 -0500

    updated pages

---
+++ context.py
@@ -0,0 +1,13 @@
+#
+# The data file name context.py in the root folder
+# will be loaded automatically into the page.
+#
+
+NUMBERS = range(5)
+
+# It may contain any valid python construct.
+
+def say_hello(name):
+    return "Hello %s" % name
+
+greeting = say_hello("World!")
